3d7500c62a712c12f8a1248fcbe3651eafa777a7
tonycheng93 <tonycheng93@outloo.com>
2018-04-04 20:27:25 +0800

    first commit

---
+++ BasicSample/app/src/main/java/com/yy/tony/basicsample/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yy.tony.basicsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author cheng
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
